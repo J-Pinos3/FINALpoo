d02d771477ab476b61dd37c47fe517556cbf8814
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2023-02-27 22:22:23 -0500

    pantalla Proveedores

---
+++ src/providerWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class providerWindow {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton CREARButton;
+    private JButton BUSCARButton;
+    private JButton LIMPIARButton;
+    private JTextField textField4;
+}
