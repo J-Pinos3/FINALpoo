b857a3e99848f91c758d18263808cda5d2774f72
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2023-02-27 22:32:35 -0500

    pantalla Roles

---
+++ src/rolWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class rolWindow {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton BUSCARButton;
+    private JButton CREARButton;
+    private JButton LIMPIARButton;
+}
