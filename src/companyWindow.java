40e6f1d559ebbeebb42c5c24bfe72ed4f9549d61
Einarr07 <aidenperse@gmail.com>
2023-02-27 21:34:09 -0500

    agregacion 2 pantallas

---
+++ src/companyWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class companyWindow {
+    private JTextField rucTField;
+    private JTextField nombreTField;
+    private JTextField cellTField;
+    private JTextField emailTField;
+    private JTextField directionTField;
+    private JPanel pantallaEmpresa;
+    private JButton guardarButton;
+    private JButton limpiarButton;
+}
---
+++ src/loginWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class loginWindow {
+    private JPanel pantallaLogin;
+    private JComboBox rolBox;
+    private JTextField usuarioTField;
+    private JTextField contraTField;
+    private JButton iniciarSecciónButton;
+}
