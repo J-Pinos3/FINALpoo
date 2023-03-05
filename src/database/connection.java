21666048427bb3c96cf13dd4ac49303e827aa79d
David Basantes <sebastian2405lucero@hotmail.com>
2023-03-04 21:28:16 -0500

    Connection

--- src/database/connection.java
+++ src/database/connection.java
@@ -4,7 +4,30 @@ import java.sql.*;
 
 public class connection{
 
+    public static Connection connection_java() {
 
+        String url = "jdbc:mysql://mysql-david2405.alwaysdata.net/david2405_poo";
+        String user = "david2405_poo";
+        String password = "POO__123@@";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        Connection con = null;
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+
+            System.out.println("Connecion exitosa");
+
+        } catch (Exception e) {
+            System.out.println("Connecion fallida");
+
+            e.getMessage();
+        }
+        return con;
+    }
+
+    /*
     public static Connection getConnection(){
 
         Connection conn = null;
@@ -21,6 +44,6 @@ public class connection{
         }
         return conn;
     }
-
+    */
 
 }
